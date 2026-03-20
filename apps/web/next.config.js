da92669bf42d48de5b979b2be331edd882c802be
LaronGovT <laron.maughn@govtech.bb>
2026-03-20 12:00:44 -0400

    chore: add Amplify hosting adapter to next.config.js for WEB_COMPUTE

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,12 @@
+const { withAmplifyHosting } = require("@aws-amplify/adapter-nextjs");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withAmplifyHosting({
   transpilePackages: [
     "@govtech-bb/form-types",
     "@govtech-bb/form-conditions",
     "@govtech-bb/form-registry",
   ],
-};
+});
 
 module.exports = nextConfig;
