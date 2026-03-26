7291f4a27bb46fd33c1382757506ec45fe42b2ea
ihtishamtanveer <ihtishamtanveer992@gmail.com>
2026-03-26 19:21:19 +0500

    feat: add TypeORM entities and update configuration for database integration

---
+++ apps/api/src/database/entities/index.ts
@@ -0,0 +1,3 @@
+export { FormComponentEntity } from "./form-component.entity";
+export { FormDefinitionEntity } from "./form-definition.entity";
+export { FormSubmissionEntity } from "./form-submission.entity";
