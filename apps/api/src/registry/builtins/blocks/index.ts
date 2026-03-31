ab6a692921c39406cfa398faefa825478b8cbc0a
Tarika Birch <tarikabirch@gmail.com>
2026-03-31 15:46:04 -0400

    Defined block exports in the blocks index

--- apps/api/src/registry/builtins/blocks/index.ts
+++ apps/api/src/registry/builtins/blocks/index.ts
@@ -1,1 +1,8 @@
-// TODO: Define built-in component definitions
+export { AdditionalInformation } from "./additional-information";
+export { ApplicantDeclaration } from "./applicant-declaration";
+export { ContactInformation } from "./contact-information";
+export { EmergencyContactDetails } from "./emergency-contact-details";
+export { PersonalInformation } from "./personal-information";
+export { PhysicalAddress } from "./physical-address";
+export { ProvingYourIdentity } from "./proving-your-identity";
+export { SupportingDocuments } from "./supporting-documents";
