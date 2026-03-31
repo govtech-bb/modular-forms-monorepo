78b30167e6a03cfc6248c74b01078da5ef21ebab
Tarika Birch <tarikabirch@gmail.com>
2026-03-31 14:07:49 -0400

    Fixed sentence casing on national insurance number component

--- apps/api/src/registry/builtins/components/national-insurance-number.ts
+++ apps/api/src/registry/builtins/components/national-insurance-number.ts
@@ -2,6 +2,6 @@ import type { TextPrimitive } from "../../types";
 
 export const NationalInsuranceNumber: TextPrimitive = {
     fieldId: "national-insurance-number",
-    label: "National Insurance Number",
+    label: "National Insurance number",
     htmlType: "text",
 }
