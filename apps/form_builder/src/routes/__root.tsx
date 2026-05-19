5640a71a9ef012076b97209fab78f28061c5abe9
IsaiahSama <zeldaplayergl@gmail.com>
2026-05-19 11:06:07 -0400

    feat(form-builder-app): scaffold app entry point and root route

---
+++ apps/form_builder/src/routes/__root.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+const RootLayout = () => (
+  <>
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({
+  component: RootLayout,
+});
